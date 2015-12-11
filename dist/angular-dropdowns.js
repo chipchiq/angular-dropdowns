c15588384471d8b6fbcf3b5888b01a06f625f2ce
chipchiq <gg@dezcom.org>
2015-12-11 15:10:45 +0300

    Update angular-dropdowns.js

--- dist/angular-dropdowns.js
+++ dist/angular-dropdowns.js
@@ -1,257 +1,245 @@
 /**
  * @license MIT http://jseppi.mit-license.org/license.html
  */
-(function (window, angular) {
-  'use strict';
+(function(window, angular, undefined) {
+	'use strict';
 
-  var dd = angular.module('ngDropdowns', []);
+	var dd = angular.module('ngDropdowns', []);
 
-  dd.run(['$templateCache', function ($templateCache) {
-    $templateCache.put('ngDropdowns/templates/dropdownSelect.html', [
-      '<div ng-class="{\'disabled\': dropdownDisabled}" class="wrap-dd-select" tabindex="0">',
-      '<span class="selected">{{dropdownModel[labelField]}}</span>',
-      '<ul class="dropdown">',
-      '<li ng-repeat="item in dropdownSelect"',
-      ' class="dropdown-item"',
-      ' dropdown-select-item="item"',
-      ' dropdown-item-label="labelField">',
-      '</li>',
-      '</ul>',
-      '</div>'
-    ].join(''));
+	dd.run(['$templateCache', function ($templateCache) {
+		$templateCache.put('ngDropdowns/templates/dropdownSelect.html', [
+			'<div class="wrap-dd-select">',
+			'<span class="selected">{{dropdownModel[labelField]}}</span>',
+			'<ul class="dropdown">',
+			'<li ng-repeat="item in dropdownSelect"',
+			' class="dropdown-item"',
+			' dropdown-select-item="item"',
+			' dropdown-item-label="labelField">',
+			'</li>',
+			'</ul>',
+			'</div>'
+		].join(''));
 
-    $templateCache.put('ngDropdowns/templates/dropdownSelectItem.html', [
-      '<li ng-class="{divider: (dropdownSelectItem.divider && !dropdownSelectItem[dropdownItemLabel]), \'divider-label\': (dropdownSelectItem.divider && dropdownSelectItem[dropdownItemLabel])}">',
-      '<a href="" class="dropdown-item"',
-      ' ng-if="!dropdownSelectItem.divider"',
-      ' ng-href="{{dropdownSelectItem.href}}"',
-      ' ng-click="selectItem()">',
-      '{{dropdownSelectItem[dropdownItemLabel]}}',
-      '</a>',
-      '<span ng-if="dropdownSelectItem.divider">',
-      '{{dropdownSelectItem[dropdownItemLabel]}}',
-      '</span>',
-      '</li>'
-    ].join(''));
+		$templateCache.put('ngDropdowns/templates/dropdownSelectItem.html', [
+			'<li ng-class="{divider: (dropdownSelectItem.divider && !dropdownSelectItem[dropdownItemLabel]), \'divider-label\': (dropdownSelectItem.divider && dropdownSelectItem[dropdownItemLabel])}">',
+			'<a href="" class="dropdown-item"',
+			' ng-if="!dropdownSelectItem.divider"',
+			' ng-href="{{dropdownSelectItem.href}}"',
+			' ng-click="selectItem()">',
+			'{{dropdownSelectItem[dropdownItemLabel]}}',
+			'</a>',
+			'<span ng-if="dropdownSelectItem.divider">',
+			'{{dropdownSelectItem[dropdownItemLabel]}}',
+			'</span>',
+			'</li>'
+		].join(''));
 
-    $templateCache.put('ngDropdowns/templates/dropdownMenu.html', [
-      '<ul class="dropdown">',
-      '<li ng-repeat="item in dropdownMenu"',
-      ' class="dropdown-item"',
-      ' dropdown-item-label="labelField"',
-      ' dropdown-menu-item="item">',
-      '</li>',
-      '</ul>'
-    ].join(''));
+		$templateCache.put('ngDropdowns/templates/dropdownMenu.html', [
+			'<ul class="dropdown">',
+			'<li ng-repeat="item in dropdownMenu"',
+			' class="dropdown-item"',
+			' dropdown-item-label="labelField"',
+			' dropdown-menu-item="item">',
+			'</li>',
+			'</ul>'
+		].join(''));
 
-    $templateCache.put('ngDropdowns/templates/dropdownMenuItem.html', [
-      '<li ng-class="{divider: dropdownMenuItem.divider, \'divider-label\': dropdownMenuItem.divider && dropdownMenuItem[dropdownItemLabel]}">',
-      '<a href="" class="dropdown-item"',
-      ' ng-if="!dropdownMenuItem.divider"',
-      ' ng-href="{{dropdownMenuItem.href}}"',
-      ' ng-click="selectItem()">',
-      '{{dropdownMenuItem[dropdownItemLabel]}}',
-      '</a>',
-      '<span ng-if="dropdownMenuItem.divider">',
-      '{{dropdownMenuItem[dropdownItemLabel]}}',
-      '</span>',
-      '</li>'
-    ].join(''));
+		$templateCache.put('ngDropdowns/templates/dropdownMenuItem.html', [
+			'<li ng-class="{divider: dropdownMenuItem.divider, \'divider-label\': dropdownMenuItem.divider && dropdownMenuItem[dropdownItemLabel]}">',
+			'<a href="" class="dropdown-item"',
+			' ng-if="!dropdownMenuItem.divider"',
+			' ng-href="{{dropdownMenuItem.href}}"',
+			' ng-click="selectItem()">',
+			'{{dropdownMenuItem[dropdownItemLabel]}}',
+			'</a>',
+			'<span ng-if="dropdownMenuItem.divider">',
+			'{{dropdownMenuItem[dropdownItemLabel]}}',
+			'</span>',
+			'</li>'
+		].join(''));
 
-  }]);
+	}]);
 
-  dd.directive('dropdownSelect', ['DropdownService',
-    function (DropdownService) {
-      return {
-        restrict: 'A',
-        replace: true,
-        scope: {
-          dropdownSelect: '=',
-          dropdownModel: '=',
-          dropdownItemLabel: '@',
-          dropdownOnchange: '&',
-          dropdownDisabled: '='
-        },
+	dd.directive('dropdownSelect', ['DropdownService',
+		function (DropdownService) {
+			return {
+				restrict: 'A',
+				replace: true,
+				scope: {
+					dropdownSelect: '=',
+					dropdownModel: '=',
+					dropdownItemLabel: '@',
+					dropdownOnchange: '&'
+				},
 
-        controller: ['$scope', '$element', function ($scope, $element) {
-          $scope.labelField = $scope.dropdownItemLabel || 'text';
+				controller: ['$scope', '$element', function ($scope, $element) {
+					$scope.labelField = $scope.dropdownItemLabel || 'text';
+					$scope.selectMap = {}
+					angular.forEach($scope.dropdownSelect, function(item) {
+						$scope.selectMap[item.value] = item;
+					});
+					console.log($scope.selectMap);
 
-          DropdownService.register($element);
+					DropdownService.register($element);
 
-          this.select = function (selected) {
-            if (!angular.equals(selected, $scope.dropdownModel)) {
-                $scope.dropdownModel = selected;
-            }
-            $scope.dropdownOnchange({
-              selected: selected
-            });
-            $element[0].blur(); //trigger blur to clear active
-          };
+					this.select = function (selected) {
+						$scope.dropdownModel = selected.value;
+						console.log($scope.dropdownModel);
+						//if (selected !== $scope.dropdownModel) {
+						//  angular.copy(selected, $scope.dropdownModel);
+						//}
+						$scope.dropdownOnchange({
+							selected: selected
+						});
+					};
 
-          $element.bind('click', function (event) {
-            event.stopPropagation();
-            if (!$scope.dropdownDisabled) {
-              DropdownService.toggleActive($element);
-            }
-          });
+					$element.bind('click', function (event) {
+						event.stopPropagation();
+						DropdownService.toggleActive($element);
+					});
 
-          $scope.$on('$destroy', function () {
-            DropdownService.unregister($element);
-          });
-        }],
-        templateUrl: 'ngDropdowns/templates/dropdownSelect.html'
-      };
-    }
-  ]);
+					$scope.$on('$destroy', function () {
+						DropdownService.unregister($element);
+					});
+				}],
+				templateUrl: 'ngDropdowns/templates/dropdownSelect.html'
+			};
+		}
+	]);
 
-  dd.directive('dropdownSelectItem', [
-    function () {
-      return {
-        require: '^dropdownSelect',
-        replace: true,
-        scope: {
-          dropdownItemLabel: '=',
-          dropdownSelectItem: '='
-        },
+	dd.directive('dropdownSelectItem', [
+		function () {
+			return {
+				require: '^dropdownSelect',
+				replace: true,
+				scope: {
+					dropdownItemLabel: '=',
+					dropdownSelectItem: '='
+				},
 
-        link: function (scope, element, attrs, dropdownSelectCtrl) {
-          scope.selectItem = function () {
-            if (scope.dropdownSelectItem.href) {
-              return;
-            }
-            dropdownSelectCtrl.select(scope.dropdownSelectItem);
-          };
-        },
+				link: function (scope, element, attrs, dropdownSelectCtrl) {
+					scope.selectItem = function () {
+						if (scope.dropdownSelectItem.href) {
+							return;
+						}
+						dropdownSelectCtrl.select(scope.dropdownSelectItem);
+					};
+				},
 
-        templateUrl: 'ngDropdowns/templates/dropdownSelectItem.html'
-      };
-    }
-  ]);
+				templateUrl: 'ngDropdowns/templates/dropdownSelectItem.html'
+			};
+		}
+	]);
 
-  dd.directive('dropdownMenu', ['$parse', '$compile', 'DropdownService', '$templateCache',
-    function ($parse, $compile, DropdownService, $templateCache) {
-      return {
-        restrict: 'A',
-        replace: false,
-        scope: {
-          dropdownMenu: '=',
-          dropdownModel: '=',
-          dropdownItemLabel: '@',
-          dropdownOnchange: '&',
-          dropdownDisabled: '='
-        },
+	dd.directive('dropdownMenu', ['$parse', '$compile', 'DropdownService', '$templateCache',
+		function ($parse, $compile, DropdownService, $templateCache) {
+			return {
+				restrict: 'A',
+				replace: false,
+				scope: {
+					dropdownMenu: '=',
+					dropdownModel: '=',
+					dropdownItemLabel: '@',
+					dropdownOnchange: '&'
+				},
 
-        controller: ['$scope', '$element', function ($scope, $element) {
-          $scope.labelField = $scope.dropdownItemLabel || 'text';
+				controller: ['$scope', '$element', function ($scope, $element) {
+					$scope.labelField = $scope.dropdownItemLabel || 'text';
 
-          var $template = angular.element($templateCache.get('ngDropdowns/templates/dropdownMenu.html'));
-          // Attach this controller to the element's data
-          $template.data('$dropdownMenuController', this);
+					var $template = angular.element($templateCache.get('ngDropdowns/templates/dropdownMenu.html'));
+					// Attach this controller to the element's data
+					$template.data('$dropdownMenuController', this);
 
-          var tpl = $compile($template)($scope);
-          var $wrap = $compile(angular.element(
-            '<div class="wrap-dd-menu" ng-class="{\'disabled\': dropdownDisabled}"></div>')
-          )($scope);
+					var tpl = $compile($template)($scope);
+					var $wrap = angular.element('<div class="wrap-dd-menu"></div>');
 
-          $element.replaceWith($wrap);
-          $wrap.append($element);
-          $wrap.append($template);
+					$element.replaceWith($wrap);
+					$wrap.append($element);
+					$wrap.append(tpl);
 
-          DropdownService.register(tpl);
+					DropdownService.register(tpl);
 
-          this.select = function (selected) {
-            if (!angular.equals(selected, $scope.dropdownModel)) {
-                $scope.dropdownModel = selected;
-            }
-            $scope.dropdownOnchange({
-              selected: selected
-            });
-          };
+					this.select = function (selected) {
+						if (selected !== $scope.dropdownModel) {
+							angular.copy(selected, $scope.dropdownModel);
+						}
+						$scope.dropdownOnchange({
+							selected: selected
+						});
+					};
 
-          $element.bind('click', function (event) {
-            event.stopPropagation();
-            if (!$scope.dropdownDisabled) {
-              DropdownService.toggleActive(tpl);
-            }
-          });
+					$element.bind('click', function (event) {
+						event.stopPropagation();
+						DropdownService.toggleActive(tpl);
+					});
 
-          $scope.$on('$destroy', function () {
-            DropdownService.unregister(tpl);
-          });
-        }]
-      };
-    }
-  ]);
+					$scope.$on('$destroy', function () {
+						DropdownService.unregister(tpl);
+					});
+				}]
+			};
+		}
+	]);
 
-  dd.directive('dropdownMenuItem', [
-    function () {
-      return {
-        require: '^dropdownMenu',
-        replace: true,
-        scope: {
-          dropdownMenuItem: '=',
-          dropdownItemLabel: '='
-        },
+	dd.directive('dropdownMenuItem', [
+		function () {
+			return {
+				require: '^dropdownMenu',
+				replace: true,
+				scope: {
+					dropdownMenuItem: '=',
+					dropdownItemLabel: '='
+				},
 
-        link: function (scope, element, attrs, dropdownMenuCtrl) {
-          scope.selectItem = function () {
-            if (scope.dropdownMenuItem.href) {
-              return;
-            }
-            dropdownMenuCtrl.select(scope.dropdownMenuItem);
-          };
-        },
+				link: function (scope, element, attrs, dropdownMenuCtrl) {
+					scope.selectItem = function () {
+						if (scope.dropdownMenuItem.href) {
+							return;
+						}
+						dropdownMenuCtrl.select(scope.dropdownMenuItem);
+					};
+				},
 
-        templateUrl: 'ngDropdowns/templates/dropdownMenuItem.html'
-      };
-    }
-  ]);
+				templateUrl: 'ngDropdowns/templates/dropdownMenuItem.html'
+			};
+		}
+	]);
 
-  dd.factory('DropdownService', ['$document',
-    function ($document) {
-      var body = $document.find('body'),
-        service = {},
-        _dropdowns = [];
+	dd.factory('DropdownService', ['$document',
+		function ($document) {
+			var body = $document.find('body'),
+				service = {},
+				_dropdowns = [];
 
-      body.bind('click', function () {
-        angular.forEach(_dropdowns, function (el) {
-          el.removeClass('active');
-        });
-      });
+			body.bind('click', function () {
+				angular.forEach(_dropdowns, function (el) {
+					el.removeClass('active');
+				});
+			});
 
-      service.register = function (ddEl) {
-        _dropdowns.push(ddEl);
-      };
+			service.register = function (ddEl) {
+				_dropdowns.push(ddEl);
+			};
 
-      service.unregister = function (ddEl) {
-        var index;
-        index = _dropdowns.indexOf(ddEl);
-        if (index > -1) {
-          _dropdowns.splice(index, 1);
-        }
-      };
+			service.unregister = function (ddEl) {
+				var index;
+				index = _dropdowns.indexOf(ddEl);
+				if (index > -1) {
+					_dropdowns.splice(index, 1);
+				}
+			};
 
-      service.toggleActive = function (ddEl) {
-        angular.forEach(_dropdowns, function (el) {
-          if (el !== ddEl) {
-            el.removeClass('active');
-          }
-        });
+			service.toggleActive = function (ddEl) {
+				angular.forEach(_dropdowns, function (el) {
+					if (el !== ddEl) {
+						el.removeClass('active');
+					}
+				});
 
-        ddEl.toggleClass('active');
-      };
+				ddEl.toggleClass('active');
+			};
 
-      service.clearActive = function () {
-        angular.forEach(_dropdowns, function (el) {
-          el.removeClass('active');
-        });
-      };
-
-      service.isActive = function (ddEl) {
-        return ddEl.hasClass('active');
-      };
-
-      return service;
-    }
-  ]);
+			return service;
+		}
+	]);
 })(window, window.angular);
